27fcf11247571b252eb96a8afa20f1bafb64fea9
enzourr21 <ty202500497@wmsu.edu.ph>
2026-03-21 00:40:36 +0800

    made void functions for input and output

--- studentGPA.cpp
+++ studentGPA.cpp
@@ -9,6 +9,37 @@ string course;
 float gpa;
 };
 
+void inputStudent(Student students[], int numStudents){
+    for (int i = 0; i < numStudents; i++){
+        cout << "==== Student " << i + 1 << " ====" << endl;
+
+        int tempID;
+        bool isDuplicate;
+        do {
+            isDuplicate = false;
+            cout << "Student ID: ";
+            cin >> tempID;
+            for (int j = 0; j < numStudents; j++){
+                if (students[j].studentID == tempID){
+                    cout << "ID already exists! Try again." << endl;
+                    isDuplicate = true;
+                    break;
+                }
+            }
+        }while (isDuplicate);
+
+        students[i].studentID = tempID;
+        cin.ignore();
+
+        cout << "Enter name: ";
+        getline (cin, students[i].name);
+        cout << "Enter course: ";
+        getline(cin, students[i].course);
+        cout << "GPA: ";
+        cin >> students[i].gpa;
+    }
+}
+
 string displayRemarks(float gpa){
     if (gpa >= 90.0 && gpa <= 100){
         return "Excellent";
@@ -27,6 +58,26 @@ string displayRemarks(float gpa){
 
 }
 
+void outputStudents(Student students[], int numStudents){
+    cout << "\n" << left << setw(15) << "Student ID"
+    << setw(20) << "Name" << setw(15) << "Course"
+    << setw(10) << "GPA" << "Remarks" << endl;
+    for (int i = 0; i < 10; i++){
+        cout << "-------";
+    }
+    cout << endl;
+    for (int i = 0; i < numStudents; i++){
+        cout << left << setw(15) << students[i].studentID << setw(20)
+        << students[i].name << setw(15)
+        << students[i].course << fixed
+        << setprecision(2) << setw(10) << students[i].gpa
+        << displayRemarks(students[i].gpa) << endl;
+    }
+        for (int i = 0; i < 10; i++){
+        cout << "-------";
+    }
+}
+
 int main() {
 char choice;
 do{
@@ -39,41 +90,9 @@ cin.ignore();
 
 Student students[numStudents];
 
-    //store
-    for (int i = 0; i < numStudents; i++){
-        cout << "\nStudent ID: ";
-        cin >> students[i].studentID;
-        cin.ignore();
-        cout << "Enter name: ";
-        getline (cin, students[i].name);
-        cout << "Enter course: ";
-        getline (cin, students[i].course);
-        cout << "GPA: ";
-        cin >> students[i].gpa;
-    }
+inputStudent(students, numStudents);
+outputStudents(students, numStudents);
 
-    //output
-     cout << left << setw(15) << "Student ID"
-     << setw(20) << "Name"
-     << setw(15) << "Course"
-     << setw(10) << "GPA"
-     << "Remarks" << endl;
-    for (int i = 0; i < 10; i++){
-        cout << "-------";
-    }
-    cout << endl;
-    for (int i = 0; i < numStudents; i++){
-        cout << left << setw(15) << students[i].studentID << setw(20)
-        << students[i].name << setw(15)
-        << students[i].course << fixed
-        << setprecision(2) << setw(10) << students[i].gpa
-        << displayRemarks(students[i].gpa) << endl;
-    }
-    for (int i = 0; i < 10; i++){
-        cout << "-------";
-    }
-
-//prompt user to repeat
 cout << "\nWould you like to repeat? ";
 cin >> choice;
 
